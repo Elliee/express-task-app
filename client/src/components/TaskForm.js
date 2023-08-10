23157d02ac8f5dec7714a002677db97d60a7ef32
Ellie <ellie.stevens@mantelgroup.com.au>
2023-08-10 14:30:16 +1000

    add delete card method + split up task components

--- client/src/components/TaskForm.js
+++ client/src/components/TaskForm.js
@@ -1,40 +1,21 @@
-import { useState } from "react";
 import styles from './TaskForm.module.css'
-const TaskForm = () => {
-    
-    const [showForm, setShowForm] = useState(true)
-    const [formData, setFormData] = useState({})
 
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const response = await fetch('http://localhost:3000/api/v1/tasks', {
-            method: 'POST',
-            body: JSON.stringify(formData),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-
-        const result = await response.json()
-        console.log(result)
-
-    }
+const TaskForm = (props) => {
 
     return (
-        <form className={styles.form} onSubmit={handleSubmit}>
+        <form className={styles.form} onSubmit={props.handleAddTask}>
             <h3>Add New task</h3>
             <label>
                 Task Name:<br></br>
                 <input
                     type="text"
-                    onChange={e => setFormData({...formData, name: e.target.value})}
+                    onChange={props.onChange}
                 >
                 </input>
             </label>
             <br></br>
             <br></br>
-            <button type="submit" onClick={() => window.location.reload(true)}>Submit</button>
+            <button type="submit">Submit</button>
         </form>
     );
 }
--- client/src/components/TaskList.js
+++ client/src/components/TaskList.js
@@ -1,40 +1,89 @@
 import { useState, useEffect } from "react";
 import styles from './TaskList.module.css'
+import TaskCard from "./TaskCard";
+import TaskForm from "./TaskForm";
+
 const TasksList = () => {
-  const [tasks, setTasks] = useState([])
-  const [loading, setLoading] = useState(false)
+  const [tasks, setTasks] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [formData, setFormData] = useState({});
+  const [showForm, setShowForm] = useState(false);
 
   const fetchTaskData = async () => {
-    setLoading(true)
+    setLoading(true);
     try {
-      const response = await fetch('http://localhost:3000/api/v1/tasks')
-      const taskData = await response.json()
-      setTasks(taskData.tasks)
+      const response = await fetch('http://localhost:3000/api/v1/tasks');
+      const taskData = await response.json();
+      setTasks(taskData.tasks);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-    setLoading(false)
+    setLoading(false);
   }
 
   useEffect(() => {
     fetchTaskData();
-  }, [])
+  }, []);
+
+  const handleDelete = async (id) => {
+    try {
+      const request = await fetch(`http://localhost:3000/api/v1/tasks/${id}`, {
+        method: "DELETE",
+      })
+      setTasks(
+        tasks.filter((task) => {
+          return task._id !== id;
+        })
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const onChange = (e) => {
+    setFormData({ ...formData, name: e.target.value });
+  }
+
+  const handleAddTask = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('http://localhost:3000/api/v1/tasks', {
+        method: 'POST',
+        body: JSON.stringify(formData),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      const result = await response.json();
+      setTasks([result.task, ...tasks]);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+    const displayForm = () => {
+    setShowForm(!showForm)
+  }
 
 
   return (
-   <div className={styles.taskContainer}>
-    {tasks && tasks.map((task, index) => {
-      return (
-        <div key={index} className={styles.taskCard}>
-          <h4>{task.name}</h4>
-          <p>Status: { task.status ? 'done' : 'not done' }</p>
-          <button>Update Task</button>
-          <button>Mark task done</button>
-        </div>
-      )
-    })}
-   </div>
-    
+    <>
+      <button onClick={displayForm}>Add Task</button>
+      { showForm &&  <TaskForm handleAddTask={handleAddTask} onChange={onChange} />}
+
+      <div className={styles.taskContainer}>
+        {tasks && tasks.map((task, index) => {
+          return (
+            <TaskCard
+              key={task._id}
+              name={task.name}
+              completed={task.completed ? 'done' : 'not done'}
+              handleDelete={() => handleDelete(task._id)}
+            />
+          )
+        })}
+      </div>
+    </>
   );
 }
 
